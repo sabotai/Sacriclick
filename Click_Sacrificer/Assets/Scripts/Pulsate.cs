adc6038ae0e728045d4096a3b4b76fcc9cc0ee13
sabotai <sabotaione@gmail.com>
2017-05-08 00:03:34 -0400

    starting out with basic layout and clicking action

---
+++ Click_Sacrificer/Assets/Scripts/Pulsate.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pulsate : MonoBehaviour {
+
+	public Vector3 startScale;
+	public Vector3 endScale;
+	public float speed = 1.0F;
+	public float startTime;
+	public float scaleSize;
+
+	// Use this for initialization
+	void Start () {
+
+		startScale = transform.localScale;
+		endScale = transform.localScale *= 0.75f;
+		startTime = Time.time;
+		scaleSize = Vector3.Distance(startScale, endScale);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/*
+	void Pulse(GameObject pulser){
+		if (Input.GetMouseButton(0)){
+			float distCovered = (Time.time - startTime) * speed;
+			float fracJourney = distCovered / scaleSize;
+			pulser.transform.localScale = Vector3.Lerp(pulser.transform.localScale, pulser.transform.localScale * 0.5f, fracJourney);
+			//pulser.transform.localScale *= 0.75f;
+		} else {
+			pulser.transform.localScale *= 1.25f;
+		}
+	}
+	*/
+
+	public static IEnumerator Pulse(GameObject pulser, float duration, float magnitude) {
+		Debug.Log ("pulsing...");
+
+
+		float elapsed = 0.0f;
+
+		Vector3 origScale = pulser.transform.localScale;
+
+		while (elapsed < duration) {
+
+			elapsed += Time.deltaTime;          
+
+			float percentComplete = elapsed / duration;         
+			float damper = 1.0f - Mathf.Clamp(4.0f * percentComplete - 3.0f, 0.0f, 1.0f);
+
+			// map value to [-1, 1]
+			float x = Random.value * 2.0f - 1.0f;
+			float y = Random.value * 2.0f - 1.0f;
+			x *= magnitude * damper;
+			y *= magnitude * damper;
+
+			pulser.transform.localScale = new Vector3(x + origScale.x, y + origScale.y, origScale.z); 
+
+			yield return null;
+		}
+
+		pulser.transform.localScale = origScale;
+	}
+
+
+	//alternative method for predefining return scale
+	public static IEnumerator Pulse(GameObject pulser, float duration, float magnitude, Vector3 definedScale) {
+		Debug.Log ("pulsing...");
+
+
+		float elapsed = 0.0f;
+
+		Vector3 origScale = definedScale;
+
+		while (elapsed < duration) {
+
+			elapsed += Time.deltaTime;          
+
+			float percentComplete = elapsed / duration;         
+			float damper = 1.0f - Mathf.Clamp(4.0f * percentComplete - 3.0f, 0.0f, 1.0f);
+
+			// map value to [-1, 1]
+			float x = Random.value * 2.0f - 1.0f;
+			float y = Random.value * 2.0f - 1.0f;
+			x *= magnitude * damper;
+			y *= magnitude * damper;
+
+			pulser.transform.localScale = new Vector3(x + origScale.x, y + origScale.y, origScale.z); 
+
+			yield return null;
+		}
+
+		pulser.transform.localScale = definedScale;
+	}
+}
---
+++ Click_Sacrificer/Assets/Scripts/Sacrifice.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sacrifice : MonoBehaviour {
+
+	public GameObject headPrefab;
+	public GameObject clickable;
+	private Vector3 clickOrigScale;
+	public AudioClip[] screams;
+	public Text sacCountDisplay;
+	AudioSource audio;
+
+	public int sacCount = 0;
+
+	// Use this for initialization
+	void Start () {
+		clickOrigScale = clickable.transform.localScale;
+		audio = GetComponent<AudioSource>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//we use rays to project invisible lines at a distance
+		//in this case, we are using the ScreenPointToRay function of our main camera
+		//to convert the mouse position on the screen into a direction projected through the screen 
+		//(in class, i drew a dinosaur on the other side)
+		Ray beam = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		//draw our debug ray so we can see inside unity
+		//the ray starts from beam.origin and is drawn to 1000 units in the rays direction (beam.direction)
+		Debug.DrawRay(beam.origin, beam.direction * 1000f, Color.red);
+
+		//declare and initialize our raycasthit to store hit information
+		RaycastHit beamHit = new RaycastHit();
+
+		//this both casts the ray "beam" and returns true if the ray hits any collider
+		//the second parameter is where our raycasthit information is stored
+		//the third parameter is how far to cast the ray
+		if (Physics.Raycast(beam, out beamHit, 1000f)){
+
+
+
+			//if the raycast hits a rigidbody and the player is pressing the right mouse button
+			if (beamHit.collider.gameObject == clickable && Input.GetMouseButton(0)){
+				//we use insideunitsphere to get a random 3D direction and multiply the direction by our power variable
+				//beamHit.rigidbody.AddForce(Random.insideUnitSphere * laserPower);
+				//pulsate = true;
+
+				//dont pulsate again before it has returned to its orig scale to prevent warping
+				//if (clickOrigScale == clickable.transform.localScale){
+				StartCoroutine(Pulsate.Pulse(beamHit.transform.gameObject, 0.15f, 0.5f, clickOrigScale));
+				audio.pitch = Random.Range(0.8f, 1.2f);
+				audio.PlayOneShot(screams[Random.Range(0, screams.Length)]);
+				sacCount++;
+				sacCountDisplay.text = "Sacrifices:  " + sacCount;
+				//}
+				Instantiate(headPrefab, beamHit.point, Quaternion.identity);
+			}
+
+		}
+
+
+	}
+
+}
