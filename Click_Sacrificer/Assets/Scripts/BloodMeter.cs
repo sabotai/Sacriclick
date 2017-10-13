0bd2e96f153e87c021ded57fd1e2c8e3c85d2692
Alec McClure <sabotaione@gmail.com>
2017-10-13 17:55:48 -0400

    blood jar model and mechanic implemented!

--- Click_Sacrificer/Assets/Scripts/BloodMeter.cs
+++ Click_Sacrificer/Assets/Scripts/BloodMeter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class BloodMeter : MonoBehaviour {
 
@@ -11,10 +12,21 @@ public class BloodMeter : MonoBehaviour {
 	public float bloodSecondRatio = 0.1f;
 	public bool failureAllowed = false;
 	public RectTransform bloodUI;
-	public float restartTimeoutAmount = 3f;
+	public float restartTimeoutAmt = 3f;
+	public float bloodJarAmt = 15f;
+	public float jarEfficiency = 0.3f;
+	public float bloodScreenAmt = 20f;
+	int bloodJarNumber = 0;
 	float bloodUIOrigY;
 	bool failed;
 	GameObject bloodCanvasItem;
+	public VideoPlayer bloodPlayer;
+	public GameObject bloodJar;
+	public Camera bloodCamera;
+	public Transform bloodSpawn;
+	public AudioClip pourSnd;
+	public AudioClip shatterSnd;
+	public AudioSource audsrc;
 
 	// Use this for initialization
 	void Start () {
@@ -29,19 +41,58 @@ public class BloodMeter : MonoBehaviour {
 		if (GetComponent<Sacrifice>().easyMode){ //use easy mode as a debug to stop blood
 			failureAllowed = false;
 
-			((MovieTexture)bloodCanvasItem.GetComponent<RawImage>().mainTexture).Stop();
+			bloodPlayer.Stop();
+			//((MovieTexture)bloodCanvasItem.GetComponent<RawImage>().mainTexture).Stop();
 		} else {
-			((MovieTexture)bloodCanvasItem.GetComponent<RawImage>().mainTexture).Play();
+			//((MovieTexture)bloodCanvasItem.GetComponent<RawImage>().mainTexture).Play();
+			bloodPlayer.Play();
 			bloodAmt -= Time.deltaTime * bloodSecondRatio; //1 ratio is 1:1 seconds to blood
 			secondsRemaining = bloodAmt / bloodSecondRatio;
 		}
 
 
 		if (bloodAmt < 0.01f && failureAllowed) failed = true; //start fail action frames
-		if (failed)	GetComponent<Sacrifice>().Fail(restartTimeoutAmount); //make fail stuff happen
+		if (failed)	GetComponent<Sacrifice>().Fail(restartTimeoutAmt); //make fail stuff happen
 		
-		bloodAmt = Mathf.Clamp(bloodAmt, 0f, 25f); //dont allow to go below zero or over 30 for ui purposes
+		if (bloodAmt > bloodScreenAmt){ //increment blood jars if enough blood is shed
+			bloodJarNumber += 1;
+			audsrc.PlayOneShot(pourSnd);
+			bloodAmt -= bloodJarAmt;
+			//GameObject newJar = Instantiate(bloodJar, bloodSpawn.position, Quaternion.identity);
+			GameObject newJar = Instantiate(bloodJar, bloodSpawn);
+			Vector3 bldSpwn = bloodSpawn.position;
+			bldSpwn += new Vector3(0,-2f,0f) * (bloodJarNumber - 1);
+			newJar.transform.position = bldSpwn;
+			//newJar.transform.position = bloodSpawn.position;
+			//newJar.transform.parent = bloodSpawn;
+
+		}
+		bloodAmt = Mathf.Clamp(bloodAmt, 0f, 20f); //dont allow to go below zero or over 30 for ui purposes
 		bloodUI.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, bloodAmt); //sets the blood movement on screen
+
+		jarCast();
+	}
+
+	void jarCast(){
+		Ray beam = bloodCamera.ScreenPointToRay(Input.mousePosition);
+
+		//declare and initialize our raycasthit to store hit information
+
+		Debug.DrawRay(beam.origin, beam.direction * 1000f, Color.red);
+		RaycastHit beamHit = new RaycastHit();
+
+		if (Input.GetMouseButtonDown(0)){
+			if (Physics.Raycast(beam, out beamHit, 1000f, LayerMask.GetMask("3D-UI"))){
+
+				if (beamHit.collider.gameObject.tag == "jar"){
+					bloodAmt += bloodJarAmt *= jarEfficiency;
+					bloodJarAmt -= 1;
+					audsrc.PlayOneShot(shatterSnd);
+					Destroy(beamHit.collider.gameObject);
+				}
+
+			}
+		}
 	}
 
 }
