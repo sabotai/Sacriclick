324be403717c52817d63d93e7b50b82f241286ca
Alec McClure <sabotaione@gmail.com>
2017-12-12 21:09:16 -0500

    useless ui testing

---
+++ Click_Sacrificer/Assets/Scripts/UITest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITest : MonoBehaviour {
+	public static string testStaticString;
+	public string testString;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void renameString(string myS){
+		testString = myS;
+	}
+}
