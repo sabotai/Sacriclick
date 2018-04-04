a16afc7b477953dc8167fbb60614b13b5ab143a6
sabotai <sabotaione@gmail.com>
2018-04-03 23:54:33 -0400

    worked on color profiling and added some screenshots

--- Click_Sacrificer/Assets/Scripts/ColorblindMode.cs
+++ Click_Sacrificer/Assets/Scripts/ColorblindMode.cs
@@ -10,11 +10,16 @@ public class ColorblindMode : MonoBehaviour {
 	public static bool cbMode;
 	public static Color cbRed = new Color(1f, 0.64f, 0f, 1f);
 	public static Color cbGreen = new Color(0f, 0.543f, 1f, 1f);
-	public Toggle cbToggle;
+	public Dropdown cbUI;
 	public PostProcessVolume[] options;
 	public static int colorOption;
 
 	void Start(){
+		//turn everything off
+		for (int i = 0; i < options.Length; i++){
+			options[i].weight = 0f;
+		}
+
 		cbGreen = new Color(0f, 0.543f, 1f, 1f);
 		cbRed = new Color(1f, 0.64f, 0f, 1f);
 		int cbInt = PlayerPrefs.GetInt("cbMode");
@@ -24,15 +29,22 @@ public class ColorblindMode : MonoBehaviour {
 			cbMode = false;
 		}
 		Debug.Log("cbMode set to: " + cbMode);
-		cbToggle.isOn = cbMode;
-		SetColor(colorOption);
+		cbUI.value = PlayerPrefs.GetInt("color");
+		SetColor(PlayerPrefs.GetInt("color"));
 	}
 	// Use this for initialization
 	public void SetColor (int setting) {
+		PlayerPrefs.SetInt("color", setting);
 		colorOption = setting;
 		cbMode = false;
 		PlayerPrefs.SetInt("cbMode", -1);
 		Camera.main.GetComponent<ContrastStretch>().enabled = false;
+
+		for (int i = 0; i < options.Length; i++){
+
+			options[i].weight = 0f;
+		}
+		if (setting < options.Length) options[setting].weight = 1f;
 
 		if (colorOption == 0){
 			Camera.main.GetComponent<ColorCorrectionCurves>().enabled = true;
@@ -42,28 +54,23 @@ public class ColorblindMode : MonoBehaviour {
 
 		} else if (colorOption == 2){
 			Camera.main.GetComponent<ContrastStretch>().enabled = true;
-		} else if (colorOption == options.Length - 1){
+		}  else if (colorOption == 3){
+			Camera.main.GetComponent<ContrastStretch>().enabled = true;
+			//options[options.Length - 1].weight = 1f;
+			options[0].weight = 1f;
+		}  else if (colorOption == 4){
+			Camera.main.GetComponent<ContrastStretch>().enabled = true;
+			options[options.Length - 1].weight = 1f;
+			options[1].weight = 1f;
+		} else if (colorOption > 4){
 			cbMode = true;
 			PlayerPrefs.SetInt("cbMode", 1);
+			options[3].weight = 1f;
 
 			//Camera.main.GetComponent<ContrastStretch>().enabled = !cbMode;
 			Camera.main.GetComponent<ColorCorrectionCurves>().enabled = false;
 
-			/*
-			if (cbMode){
-				options[setting].weight = 0f;
-				cbVol.weight = 1f;
-			} else {
-				defaultVol.weight = 1f;
-				cbVol.weight = 0f;
-			}
-			*/
 		}
-		for (int i = 0; i < options.Length; i++){
-
-			options[i].weight = 0f;
-		}
-		options[setting].weight = 1f;
 	}
 	
 	// Update is called once per frame
