750d5e8b348ef7c748b8c914b09d98de10e5a417
sabotai <sabotaione@gmail.com>
2018-04-13 00:23:32 -0400

    added cheats and fixed a ui bug

---
+++ Click_Sacrificer/Assets/Scripts/Cheat.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cheat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.F12) && Input.GetKeyDown(KeyCode.F1))
+			SceneManager.LoadScene(0);
+		if (Input.GetKey(KeyCode.F12) && Input.GetKeyDown(KeyCode.F2))
+			SceneManager.LoadScene(1);
+		if (Input.GetKey(KeyCode.F12) && Input.GetKeyDown(KeyCode.G))
+			Camera.main.GetComponent<Sacrifice>().easyMode = !Camera.main.GetComponent<Sacrifice>().easyMode;
+	}
+}
